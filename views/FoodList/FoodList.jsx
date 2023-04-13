46e87e4fddb1c325ae34934bcf8aa0e623aa32e6
tuan0342 <79151156+tuan0342@users.noreply.github.com>
2023-04-13 15:55:17 +0700

    video 9 food screen (danh sách FlatList kiểu ngang)

--- views/FoodList/FoodList.jsx
+++ views/FoodList/FoodList.jsx
@@ -1,9 +1,18 @@
 import React, {useState, useEffect} from 'react';
-import {View, StyleSheet, ScrollView, FlatList} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 import FoodItem from './FoodItem';
+import {colors, fontSize} from '../../constants/controlConst';
 
 function FoodList(props) {
-  //list of foods = state
+  // list of foods = state
   const [foods, setFoods] = useState([
     {
       name: 'Paella Valenciana, with rabbit and chicken; and seafood paella',
@@ -71,8 +80,80 @@ function FoodList(props) {
     },
   ]);
 
+  // list of category
+  const [categories, setCategories] = useState([
+    {
+      name: 'BBQ',
+      url: 'https://cdn-icons-png.flaticon.com/512/3313/3313199.png',
+    },
+    {
+      name: 'Breakfast',
+      url: 'https://cdn-icons-png.flaticon.com/512/4825/4825292.png',
+    },
+    {
+      name: 'Coffee',
+      url: 'https://static.vecteezy.com/system/resources/previews/010/160/674/non_2x/coffee-icon-sign-symbol-design-free-png.png',
+    },
+    {
+      name: 'Noodles',
+      url: 'https://cdn-icons-png.flaticon.com/512/3041/3041130.png',
+    },
+    {
+      name: 'Hotdogs',
+      url: 'https://cdn-icons-png.flaticon.com/512/3480/3480814.png',
+    },
+    {
+      name: 'Dinner',
+      url: 'https://www.clipartmax.com/png/middle/114-1147320_open-source-lunch-lunch-and-dinner-icon.png',
+    },
+    {
+      name: 'Beverage',
+      url: 'https://img.lovepik.com/element/40027/7097.png_1200.png',
+    },
+    {
+      name: 'Besser',
+      url: 'https://cdn-icons-png.flaticon.com/512/2488/2488456.png',
+    },
+    {
+      name: 'Wine',
+      url: 'https://cdn-icons-png.flaticon.com/512/763/763072.png',
+    },
+  ]);
+
   return (
     <View style={{flex: 1, backgroundColor: 'white'}}>
+      {/* Filter */}
+      <View style={{height: 60}}></View>
+
+      {/* FlatList ngang */}
+      <View style={{height: 80}}>
+        <View style={{height: 1, backgroundColor: colors.lighBlack}}></View>
+        <FlatList
+          style={{flex: 1}}
+          horizontal={true}
+          data={categories}
+          renderItem={({item}) => {
+            return (
+              <TouchableOpacity
+                onPress={() => {
+                  alert(`Press: ${item.name}`);
+                }}
+                style={{justifyContent: 'center', alignItems: 'center'}}>
+                <Image
+                  style={styles.FlatListHorizontalImg}
+                  source={{uri: item.url}}
+                />
+                <Text style={{color: 'black', fontSize: fontSize.h6}}>
+                  {item.name}
+                </Text>
+              </TouchableOpacity>
+            );
+          }}
+        />
+        <View style={{height: 1, backgroundColor: colors.lighBlack}}></View>
+      </View>
+
+      {/* FlatList dọc */}
       {/* <ScrollView>
         {foods.map(eachFood => (
           <FoodItem food={eachFood} key={eachFood.name} />
@@ -94,6 +175,15 @@ function FoodList(props) {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  FlatListHorizontalImg: {
+    width: 45,
+    height: 45,
+    resizeMode: 'cover',
+    borderRadius: 30,
+    marginHorizontal: 10,
+    marginTop: 3,
+  },
+});
 
 export default FoodList;
